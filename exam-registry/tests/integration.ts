f5ee691a8662cd5b7332f8142dd3aa2321dcefe5
natalijapesic <natalijapesic@elfak.rs>
2023-02-12 14:12:15 +0100

    feat: contract uploaded on testnest

---
+++ exam-registry/tests/integration.ts
@@ -0,0 +1,170 @@
+import axios from "axios";
+import { Wallet, SecretNetworkClient } from "secretjs";
+import fs from "fs";
+
+// Returns a client with which we can interact with secret network
+const initializeClient = async (endpoint: string, chainId: string) => {
+  const wallet = new Wallet(
+    "hard trust royal express upper theory garden rotate reform north club permit stove add actress globe bronze finish unknown question dinosaur dash brain way"
+  ); // Use default constructor of wallet to generate random mnemonic.
+  const accAddress = wallet.address;
+  const client = new SecretNetworkClient({
+    // Create a client to interact with the network
+    url: endpoint,
+    chainId: chainId,
+    wallet: wallet,
+    walletAddress: accAddress,
+  });
+
+  console.log(`Initialized client with wallet address: ${accAddress}`);
+
+  return client;
+};
+
+// Stores and instantiaties a new contract in our network
+const initializeContract = async (
+  client: SecretNetworkClient,
+  contractPath: string
+) => {
+  const wasmCode = fs.readFileSync(contractPath);
+  console.log("Uploading contract");
+  console.log(wasmCode.length);
+
+  const uploadReceipt = await client.tx.compute.storeCode(
+    {
+      wasm_byte_code: wasmCode,
+      sender: client.address,
+      source: "",
+      builder: "",
+    },
+    {
+      gasLimit: 5000000,
+    }
+  );
+
+  if (uploadReceipt.code !== 0) {
+    console.log(
+      `Failed to get code id: ${JSON.stringify(uploadReceipt.rawLog)}`
+    );
+    throw new Error(`Failed to upload contract`);
+  }
+
+  const codeIdKv = uploadReceipt.jsonLog![0].events[0].attributes.find(
+    (a: any) => {
+      return a.key === "code_id";
+    }
+  );
+
+  const codeId = Number(codeIdKv!.value);
+  console.log("Contract codeId: ", codeId);
+
+  const contractCodeHash = (
+    await client.query.compute.codeHashByCodeId({ code_id: String(codeId) })
+  ).code_hash;
+
+  if (contractCodeHash === undefined) {
+    throw new Error(`Failed to get code hash`);
+  }
+
+  console.log(`Contract hash: ${contractCodeHash}`);
+
+  const contract = await client.tx.compute.instantiateContract(
+    {
+      sender: client.address,
+      code_id: codeId,
+      init_msg: { name: "test" }, // Initialize our counter to start from 4. This message will trigger our Init function
+      code_hash: contractCodeHash,
+      label: "My contract" + Math.ceil(Math.random() * 10000), // The label should be unique for every contract, add random string in order to maintain uniqueness
+    },
+    {
+      gasLimit: 1000000,
+    }
+  );
+
+  if (contract.code !== 0) {
+    throw new Error(
+      `Failed to instantiate the contract with the following error ${contract.rawLog}`
+    );
+  }
+
+  const contractAddress = contract.arrayLog!.find(
+    (log) => log.type === "message" && log.key === "contract_address"
+  )!.value;
+
+  console.log(`Contract address: ${contractAddress}`);
+
+  const contractInfo: [string, string] = [contractCodeHash, contractAddress];
+  return contractInfo;
+};
+
+const getFromFaucet = async (address: string) => {
+  await axios.get(`https://faucet.pulsar.scrttestnet.com?address=${address}`);
+};
+
+async function getScrtBalance(userCli: SecretNetworkClient): Promise<string> {
+  let balanceResponse = await userCli.query.bank.balance({
+    address: userCli.address,
+    denom: "uscrt",
+  });
+
+  if (balanceResponse?.balance?.amount === undefined) {
+    throw new Error(`Failed to get balance for address: ${userCli.address}`);
+  }
+
+  return balanceResponse.balance.amount;
+}
+async function fillUpFromFaucet(
+  client: SecretNetworkClient,
+  targetBalance: Number
+) {
+  let balance = await getScrtBalance(client);
+  while (Number(balance) < targetBalance) {
+    try {
+      await getFromFaucet(client.address);
+    } catch (e) {
+      console.error(`failed to get tokens from faucet: ${e}`);
+    }
+    balance = await getScrtBalance(client);
+  }
+  console.error(`got tokens from faucet: ${balance}`);
+}
+
+// Initialization procedure
+async function initializeAndUploadContract() {
+  let endpoint = "https://api.pulsar.scrttestnet.com";
+  let chainId = "pulsar-2";
+
+  const client = await initializeClient(endpoint, chainId);
+
+  const [contractHash, contractAddress] = await initializeContract(
+    client,
+    "contract.wasm"
+  );
+
+  var clientInfo: [SecretNetworkClient, string, string] = [
+    client,
+    contractHash,
+    contractAddress,
+  ];
+  return clientInfo;
+}
+
+async function runTestFunction(
+  tester: (
+    client: SecretNetworkClient,
+    contractHash: string,
+    contractAddress: string
+  ) => void,
+  client: SecretNetworkClient,
+  contractHash: string,
+  contractAddress: string
+) {
+  console.log(`Testing ${tester.name}`);
+  await tester(client, contractHash, contractAddress);
+  console.log(`[SUCCESS] ${tester.name}`);
+}
+
+(async () => {
+  const [client, contractHash, contractAddress] =
+    await initializeAndUploadContract();
+})();
